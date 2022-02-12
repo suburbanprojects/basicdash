3aacd816e8f5d2b1acc494b177b90b49b7cca836
suburbanprojects <96915573+suburbanprojects@users.noreply.github.com>
2022-02-12 23:28:54 +0800

    Update basicdash.py

--- basicdash.py
+++ basicdash.py
@@ -7,16 +7,18 @@ import time
 #import regular expressions
 import re
 
-#get current month and year from host 
-today = datetime.date.today()
-year = today.year
-month = today.month
-currentmonth = calendar.month(year,month)
-date = today.day.__str__().rjust(2)
-rday  = ('\\b' + date + '\\b').replace('\\b ', '\\s')
-#7 Swaps foreground and background colors and print the current date
-rdayc = "\033[7m" + date + "\033[0m"
-print(colored(re.sub(rday,rdayc,currentmonth),"yellow"))
+
+def showcal():
+    #get current month and year from host
+    today = datetime.date.today()
+    year = today.year
+    month = today.month
+    currentmonth = calendar.month(year,month)
+    date = today.day.__str__().rjust(2)
+    rday  = ('\\b' + date + '\\b').replace('\\b ', '\\s')
+    #7 Swaps foreground and background colors and print the current date
+    rdayc = "\033[7m" + date + "\033[0m"
+    print(colored(re.sub(rday,rdayc,currentmonth),"yellow"))
 
 #define the clock function
 def clock():
@@ -25,8 +27,9 @@ def clock():
         print(colored(datetime.datetime.now().strftime("The time is: " + "%H:%M:%S"),"blue"),end="\r")
         #the program waits for 1 second before printing the latest time 
         time.sleep(1)
-        
-#run the function
+
+#run the functions
+showcal()
 clock()
 
 #the calendar highlight stuff was borrowed from shorturl.at/clzO9
